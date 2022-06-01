1c4e4899ad7263356a20ced5a57b491d2441ecfa
DojcsakEdina <100783729+DojcsakEdina@users.noreply.github.com>
2022-06-01 21:37:32 +0200

    Add files via upload
    számlázó beadandó

---
+++ szamlazo/Models/szamla.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szamlazo.Models
+{
+    public class Szamla
+    {
+        private double netto;
+        public Szamla(double netto)
+        {
+            this.netto = netto;
+        }
+        public double Netto
+        {
+            get 
+            { 
+                return netto; 
+            }
+            set
+            {
+                netto = value;
+            }
+        }
+        public double Brutto
+        {
+            get
+            {
+                double brutto = netto + (netto * 0.27);
+                return brutto;
+            }
+        }
+
+        public override string ToString()
+        {
+            string result = "A termék ára: " + Netto + " Ft\n";
+            result += "Az Áfa: " + (netto * 0.27) + " Ft\n";
+            result += "Bruttó ár: " + Brutto + " Ft\n";
+            return result;
+        }
+    }
+}
---
+++ szamlazo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using szamlazo.Models;
+
+namespace szamlazo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Szamla sz1 = new Szamla(5600);
+            Console.WriteLine(sz1);
+            Console.ReadKey();
+        }
+    }
+}
